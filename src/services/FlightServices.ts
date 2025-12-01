25dd2a5ae2b3be7f7f49c3901d7f7c4b8a75296f
yasiqb89 <mian.yassir@gmail.com>
2025-12-01 17:14:51 +0100

    fix: Correct file path and add module execution test block.

--- src/services/FlightServices.ts
+++ src/services/FlightServices.ts
@@ -1,7 +1,7 @@
 import { readFile, writeFile } from "fs/promises";
 import Flight, { FlightData } from "../models/Flight.js"
 
-const filePath = new URL('../../ data / flight.json', import.meta.url);
+const filePath = new URL('../../data/flight.json', import.meta.url);
 
 export async function getAllFlights(): Promise<Flight[]> {
     try {
@@ -16,3 +16,15 @@ export async function getAllFlights(): Promise<Flight[]> {
 }
 
 
+if (import.meta.url === `file://${process.argv[1]}`) {
+    console.log("Running Flight test...");
+
+    const result = await getAllFlights();
+    console.log("Loaded flights:", result.length);
+
+    if (result[0]) {
+        console.log("First flight info:", result[0].info);
+    } else {
+        console.log("No flights found.");
+    }
+}
